5cc7d9229128cefdc1435cf1423b17648f54a54e
Raphael Roshan <raphaelroshan@gmail.com>
2020-07-02 23:08:55 +0800

    working binary

--- robomaster/predict_binary.py
+++ robomaster/predict_binary.py
@@ -2,15 +2,29 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# image = cv2.imread("VideoFrame_uncropped2.jpg")
+model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\RESNET50_binary_snapshot_01.h5")
+
+def predict_binary(image):
+        image = cv2.resize(image, (299, 299))
+        image = np.array(image).reshape((1, 299, 299, 3))
+        res = model.predict(image)   
+        if res >= 0.5:
+            pred = 1
+        else:
+            pred = 0
+        return res, pred
+
+# image_list = ["VideoFrame_uncropped23.jpg","VideoFrame_uncropped2.jpg","VideoFrame_uncropped261.jpg","VideoFrame_uncropped284.jpg","VideoFrame_uncropped559.jpg","VideoFrame_uncropped580.jpg","VideoFrame_uncropped1269.jpg","VideoFrame_uncropped1345.jpg"]
+
+# for images in image_list:
+#     image = cv2.imread(images)
+#     #image = cv2.imread("VideoFrame_uncropped580.jpg")
 
 
-model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\Xception_binary_snapshot_01.h5")
-res = model.predict(image)
-def detect_binary(image):
-    image = cv2.resize(image, (1280, 1280))
-    image = np.array(image).reshape((1, 1280, 1280, 3))
-    res = model.predict(image)   
-    return res
+#     #model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\Xception_binary_snapshot_01.h5")
+#     #model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\RESNET50_binary_snapshot_01.h5")
 
-# print(predict_binary(image))
+#     #res = model.predict(image)
+    
+
+#     print(predict_binary(image))

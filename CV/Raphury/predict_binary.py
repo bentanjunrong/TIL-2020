c4625bd68a833da4f5548303f485c3444c2da001
Raphael Roshan <raphaelroshan@gmail.com>
2020-07-02 13:45:44 +0800

    binary predict

---
+++ CV/Raphury/predict_binary.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+# image = cv2.imread("VideoFrame_uncropped2.jpg")
+
+
+model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\Xception_binary_snapshot_01.h5")
+res = model.predict(image)
+def predict_binary(image):
+    image = cv2.resize(image, (1280, 1280))
+    image = np.array(image).reshape((1, 1280, 1280, 3))
+    res = model.predict(image)   
+    return res
+
+# print(predict_binary(image))

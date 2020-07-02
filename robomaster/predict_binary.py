c7f6895c930636ef52e9bcc302e90c3f9e7250f1
Raphael Roshan <raphaelroshan@gmail.com>
2020-07-02 23:18:43 +0800

    updated_resize

--- robomaster/predict_binary.py
+++ robomaster/predict_binary.py
@@ -5,26 +5,11 @@ import tensorflow as tf
 model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\RESNET50_binary_snapshot_01.h5")
 
 def predict_binary(image):
-        image = cv2.resize(image, (299, 299))
-        image = np.array(image).reshape((1, 299, 299, 3))
-        res = model.predict(image)   
-        if res >= 0.5:
-            pred = 1
-        else:
-            pred = 0
-        return res, pred
-
-# image_list = ["VideoFrame_uncropped23.jpg","VideoFrame_uncropped2.jpg","VideoFrame_uncropped261.jpg","VideoFrame_uncropped284.jpg","VideoFrame_uncropped559.jpg","VideoFrame_uncropped580.jpg","VideoFrame_uncropped1269.jpg","VideoFrame_uncropped1345.jpg"]
-
-# for images in image_list:
-#     image = cv2.imread(images)
-#     #image = cv2.imread("VideoFrame_uncropped580.jpg")
-
-
-#     #model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\Xception_binary_snapshot_01.h5")
-#     #model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\RESNET50_binary_snapshot_01.h5")
-
-#     #res = model.predict(image)
-    
-
-#     print(predict_binary(image))
+    image = cv2.resize(image, (224, 224))
+    image = np.array(image).reshape((1, 224, 224, 3))
+    res = model.predict(image)   
+    if res >= 0.5:
+        pred = 1
+    else:
+        pred = 0
+    return pred #set to res for probability

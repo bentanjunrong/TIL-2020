8aa4ce2584d107280a075e4a14631cb8f3f49f23
Raphael Roshan <raphaelroshan@gmail.com>
2020-07-03 18:47:33 +0800

    fixed_color

--- robomaster/predict_binary.py
+++ robomaster/predict_binary.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 model = tf.keras.models.load_model("C:\\Users\\Raphael\\Documents\\TIL_yolo_model\\RESNET50_binary_snapshot_01.h5")
 
 def predict_binary(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (224, 224))
     image = np.array(image).reshape((1, 224, 224, 3))
     res = model.predict(image)   

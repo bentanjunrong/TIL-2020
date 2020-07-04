c2bd95e6a8a71118dcd28ba30e7f650cd6b5f850
RavyuS <ravyusk@gmail.com>
2020-07-04 11:40:57 +0800

    ocr_try

---
+++ CV/Raphury/OCR_try2.py
@@ -0,0 +1,146 @@
+import numpy as np
+#import argparse
+#import imutils
+import time
+import cv2
+
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+	# initialize the list of picked indexes	
+	pick = []
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+	# compute the area of the bounding boxes and sort the bounding
+	# boxes by the bottom-right y-coordinate of the bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+		# find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return boxes[pick].astype("int")
+
+def decode_predictions(scores, geometry):
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the
+		# geometrical data used to derive potential bounding box
+		# coordinates that surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability,
+			# ignore it
+			if scoresData[x] < 0.5:
+				continue
+			# compute the offset factor as our resulting feature
+			# maps will be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+			# extract the rotation angle for the prediction and
+			# then compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+			# use the geometry volume to derive the width and height
+			# of the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+			# compute both the starting and ending (x, y)-coordinates
+			# for the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+			# add the bounding box coordinates and probability score
+			# to our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+	# return a tuple of the bounding boxes and associated confidences
+	return (rects, confidences)
+
+(W, H) = (None, None)
+(newW, newH) = (320,320)
+(rW, rH) = (None, None)
+
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet("/2TBRaid/code/hackathon/TIL/notebooks/models/frozen_east_text_detection.pb")
+print("done loading")
+
+frame = cv2.imread("/2TBRaid/code/hackathon/TIL/notebooks/models/test.jpg")
+
+orig = frame.copy()
+# if our frame dimensions are None, we still need to compute the
+# ratio of old frame dimensions to new frame dimensions
+if W is None or H is None:
+    (H, W) = frame.shape[:2]
+    rW = W / float(newW)
+    rH = H / float(newH)
+# resize the frame, this time ignoring aspect ratio
+frame = cv2.resize(frame, (newW, newH))
+
+blob = cv2.dnn.blobFromImage(frame, 1.0, (newW, newH),
+    (123.68, 116.78, 103.94), swapRB=True, crop=False)
+net.setInput(blob)
+(scores, geometry) = net.forward(layerNames)
+# decode the predictions, then  apply non-maxima suppression to
+# suppress weak, overlapping bounding boxes
+(rects, confidences) = decode_predictions(scores, geometry)
+print("rects",rects)
+boxes = non_max_suppression_fast(np.array(rects), overlapThresh=0.3)
+# loop over the bounding boxes
+print("looping", boxes)
+for (startX, startY, endX, endY) in boxes:
+    # scale the bounding box coordinates based on the respective
+    # ratios
+    startX = int(startX * rW)
+    startY = int(startY * rH)
+    endX = int(endX * rW)
+    endY = int(endY * rH)
+    print(startX, startY, endX, endY)
+    # draw the bounding box on the frame
+    cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
